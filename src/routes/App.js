e4863a8be60866ecb327f72a4ce2e9119184e697
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-08 15:55:18 -0500

    Add Routes/login

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 //Encapusula los componentes que necesitamos para trabajar con la ruta
 import { BrowserRouter, Route} from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/Login';
 
 const App = () => (
     <BrowserRouter>
      <Route exact path="/" component={Home}/>
+     <Route exact path="/Login" component={Login}/>
     </BrowserRouter>
 )
 
