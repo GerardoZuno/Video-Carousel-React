e796f342be64c54ddf6cbe680af90233803e42b0
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-08 16:33:27 -0500

    Add Routes/Register

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 //Encapusula los componentes que necesitamos para trabajar con la ruta
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
 
 const App = () => (
+
     <BrowserRouter>
+    //switch sirve para solo mostrar el elemento con el que se 
+     este haciendo match
+     <Switch>
      <Route exact path="/" component={Home}/>
      <Route exact path="/Login" component={Login}/>
+     <Route exact path="/Register" component={Register}/>
+     </Switch>
     </BrowserRouter>
 )
 
