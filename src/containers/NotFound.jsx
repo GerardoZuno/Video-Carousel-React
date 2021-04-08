9da1daf2855f6107f5e6a99055c0c1e54ced09c2
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-08 16:57:38 -0500

    Add Routes/NotFound

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss'
+
+
+const NotFound = () => (
+    <main className="main">
+    <div className="wrap"/>
+<div className="box">
+  <div className="text animated bounce">Error404</div>
+</div>
+</main>
+);
+
+
+export default NotFound
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,17 +3,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import NotFound from '../containers/NotFound';
 import Register from '../containers/Register';
 
 const App = () => (
 
     <BrowserRouter>
-    //switch sirve para solo mostrar el elemento con el que se 
-     este haciendo match
      <Switch>
      <Route exact path="/" component={Home}/>
      <Route exact path="/Login" component={Login}/>
      <Route exact path="/Register" component={Register}/>
+     <Route component={NotFound}/>
      </Switch>
     </BrowserRouter>
 )
