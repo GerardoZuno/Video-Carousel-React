db3c8514fa1d9f10287dea25f66c036446a46db7
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-16 16:35:40 -0500

    Validation for login and logout

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,12 @@ export const setFavorite = payload => ({
  })
 
  export const loginRequest = payload => ({
-    type: 'LOGIN_REQ',
+    type: 'LOGIN_REQUEST',
+    payload,
+ })
+
+ export const logoutRequest = payload => ({
+    type: 'LOGOUT_REQUEST',
     payload,
  })
 
