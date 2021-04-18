30b0490d00c7fab6d1f11f3e39af69a72984d21c
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-18 15:02:50 -0500

    Player Completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,8 +17,13 @@ export const setFavorite = payload => ({
     type: 'LOGOUT_REQUEST',
     payload,
  })
- 
+
  export const registerRequest = payload => ({
     type: 'REGISTER_REQUEST',
     payload,
+ })
+
+ export const getVideoSource = payload => ({
+     type: 'GET_VIDEO_SOURCE',
+     payload,
  })
--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
-import  '../assets/styles/components/Player.scss'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { getVideoSource } from '../actions'
 
+import NotFound from './NotFound'
 
+import '../assets/styles/components/Player.scss'
 
 const Player = props => {
-    const { id } = props.match.params
-    return(
-      <div className="Player">
-        <video controls autoPlay>
-            <source src="" type="video/mp4"/>
-        </video>
-        <div className="Player-back" onClick={() => props.history.goBack()}>
-            <button type="button">
-                Regresar
-            </button>
+  const { id } = props.match.params
+  const [ loading, setLoading ] = useState(true)
+  const hasPlaying = Object.keys(props.playing).length > 0
 
-        </div>
-      </div>
-    )
+  useEffect(() => {
+    props.getVideoSource(id)
+    setLoading(false)
+  }, [])
+
+  if (loading) return <h2>Cargando video...</h2>
+  
+  return hasPlaying ? (
+		<div className="player">
+			<video controls autoPlay>
+				<source src={props.playing.source} type="video/mp4" />
+			</video>
+			<div className="Player-back">
+				<button type="button" onClick={() => props.history.goBack()}>
+					Regresar
+				</button>
+			</div>
+		</div>
+	) : <NotFound />
 }
 
+const mapStateToProps = state => (
+  {
+    playing: state.playing
+  }
+)
 
+const mapDispatchToProps = {
+	getVideoSource,
+}
 
-
-
-export default Player
+export default connect(mapStateToProps, mapDispatchToProps)(Player)
