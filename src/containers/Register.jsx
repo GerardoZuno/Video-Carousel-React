ad92c0695a7aafa1db60f6e1d97d2427ac1b7c89
Gerardo Zuno <gerardozunounitec@gmail.com>
2021-04-19 14:44:00 -0500

    Add Validations

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { registerRequest } from '../actions';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header'
 import '../assets/styles/components/Register.scss';
 
 
@@ -25,10 +26,12 @@ const Register = props => {
   }
 
   return (
-    <section className="register">
-    <section className="register__container">
-      <h2>Regístrate</h2>
-      <form className="register__container--form" onSubmit={handleSubmit}>
+   <>
+     <Header isRegister/> 
+     <section className="register">
+     <section className="register__container">
+       <h2>Regístrate</h2>
+       <form className="register__container--form" onSubmit={handleSubmit}>
         <input 
          name="name"
          className="input" 
@@ -44,20 +47,21 @@ const Register = props => {
           placeholder="Correo"
           onChange={handleInput}
            />
-        <input 
+       <input 
           name="password"
           className="input"
           type="password" 
           placeholder="Contraseña"
           onChange={handleInput}
            />
-        <button className="button">Registrarme</button>
-      </form>
-      <Link to="/login">
-       Iniciar sesión
-      </Link>
+       <button className="button">Registrarme</button>
+       </form>
+       <Link to="/login">
+        Iniciar sesión
+       </Link>
+     </section>
     </section>
-  </section>
+  </>
 )
   }
 
